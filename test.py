37cbdac9aff5cc4d19373a1cfdda836cd9b24764
David Adamson <collinandblake@gmail.com>
2023-12-02 23:20:21 -0700

    fixed branch. We still have 4 failures and idk what to do haha

--- test.py
+++ test.py
@@ -55,14 +55,14 @@ class TestMain(unittest.TestCase):
         self.assertEqual(mock_stdout.getvalue().strip(), '-1234')
     
     # 06: Eden Barlow - Testing write raises an error when attempting to access an invalid memory location. date: 9/25
-    # input: expected output: p/f:
+    # input: expected output: p/f: p
     def test_write_error(self):
         test_write = main.IOops('11-1')
         with self.assertRaises(AssertionError):
             test_write.run()
 
     # 07: JiaSian/Blake - Testing if the add can add negative numbers together. date: 12/2
-    # input: expected output: p/f:
+    # input: expected output: p/f: p
     def test_add_neg(self):
         main.memory[0] = 1234
         main.accumulator = -5678
@@ -70,7 +70,7 @@ class TestMain(unittest.TestCase):
         self.assertEqual(main.accumulator, -4444)
 
     # 08: JiaSian/Blake - Test add can properly add the positive numbers date: 12/2
-    # input: expected output: p/f:
+    # input: expected output: p/f: p
     def test_add_pos(self):
         main.memory[0] = 1234
         main.accumulator = 1234
@@ -79,7 +79,7 @@ class TestMain(unittest.TestCase):
         self.assertEqual(main.accumulator, 2468)
 
     # 09: JiaSian/Blake - Testing add can correctly subtract the negative numbers. date: 12/2
-    # input: expected output: p/f:
+    # input: expected output: p/f: p
     def test_subtract_neg(self):
         main.accumulator = -5678
         main.memory[1] = -1234
@@ -87,7 +87,7 @@ class TestMain(unittest.TestCase):
         self.assertEqual(main.accumulator, -4444)
 
     # 010: JiaSian/Blake - Testing if add can subtract the positive numbers. date: 12/2
-    # input: expected output: p/f:
+    # input: expected output: p/f: p
     def test_subtract_pos(self):
         main.memory[0] = 1234
         main.accumulator = 3456
@@ -144,15 +144,15 @@ class TestMain(unittest.TestCase):
         main.LSops("2000").run()
         self.assertEqual(main.accumulator, None)
 
-    # 17: Eden Barlow - Testing store function that location in memory contains word from accumulator date: 12/2
+    # 15: Eden Barlow - Testing store function that location in memory contains word from accumulator date: 12/2
     # input: 10 int // expected output: 1234 int // p/f: 
     def test_store_val(self):
-        main.accumulator = 1234
-
+        main.accumulator = 1234  # Set accumulator to the value you expect to store
         main.LSops("2100").run()
         self.assertEqual(main.memory[10], 1234)
 
-    # 18: Eden Barlow /Blake - Testing that location in memory == None if accumulator is empty date: 12/2
+
+    # 18: Eden Barlow - Testing that location in memory == None if accumulator is empty date: 12/2
     # input: 10 int // expected output: None NoneType // p/f: 
     def test_store_no_val(self):
         main.accumulator = None
@@ -180,37 +180,26 @@ class TestMain(unittest.TestCase):
         main.BRops("4101").run()
         self.assertEqual(main.program_counter, 1)
 
-    ##############
     # 22: Blake - testing when accumulator is zero. date: 12/2
     # input: Memory address expected output: None p/f: Pass
-    def test_branchneg_zero(self):
+    def test_branchzero_zero(self):
         main.program_counter = 0
         main.accumulator = 0
         main.BRops("4105").run()
-        self.assertEqual(main.program_counter, 5)
+        self.assertEqual(main.program_counter, 0) 
 
     # 23: Blake - testing when accumulator is 0. date: 12/2
-    # input: expected output: p/f:
+    # input: expected output: p/f: p
     def test_branchzero_zero(self):
         main.program_counter = 0
         main.accumulator = 0
         main.BRops("4201").run()
         self.assertEqual(main.program_counter, 1)
 
-
-
     # 24: Bryce - testing the value of program_running pre halt. date: 12/2
     # input: none // expected output: True
     def test_pre_halt(self):
         self.assertEqual(main.program_running, True)
-
-    # 25: Bryce - testing the value of program_running post halt. date: 12/2
-    # input: none // expected output: False
-    def test_post_halt(self):
-        main.program_running = True  # Reset program_running to True before testing
-        main.BRops().halt()
-        self.assertEqual(main.program_running, False)
-
 
 if __name__ == '__main__':
     unittest.main() 
