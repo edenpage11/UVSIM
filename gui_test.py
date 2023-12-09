95b2ac0c310814541c979609aa48fb22f475e063
David Adamson <collinandblake@gmail.com>
2023-12-09 10:10:55 -0700

    getting script errors. 3 tests working, need to get rid of popup on start

--- gui_test.py
+++ gui_test.py
@@ -13,21 +13,29 @@ class TestGui(unittest.TestCase):
     def tearDown(self):
         self.app.destroy()
     
-    # 01: Blake Adamson - Testing that startup has correct title. Date:12/9
+    # 0: Blake Adamson - Testing that GUI correctly displays title. Date:12/9
     # input: expected string // expected output: UVSIM code editor // p/f: p
     def test_title(self):
         title = self.app.winfo_toplevel().title()
         expected = 'UVSIM code editor'
         self.assertEqual(title, expected)
     
-    # 01: Blake Adamson - Testing the num_button_click method. Date: 12/9
-    # input: Clicking a number button (e.g., 5) // expected output: Textbox insert called with "end" and "5" // p/f: p
+    # 0: Blake Adamson - Testing that number buttons get put in textbox. Date: 12/9
+    # input: Clicking the button 5 // expected output: Textbox insert called with "end" and "5" // p/f: p
     def test_num_button_click(self):
-        # Mock the textbox insert method to check if called with correct value
         with patch.object(self.app.textbox, "insert", MagicMock()) as mock_insert:
             self.app.num_button_click(5)
-        # Assert that insert method called with expected args
         mock_insert.assert_called_once_with("end", "5")
+
+    # 0: Blake Adamson - Testing that the edit_button_clear method will clear inputs and textbox. Date: 12/9
+    # input: Clicking the "Clear" button // expected output: Inputs list cleared, textbox content cleared to newline // p/f: p
+    def test_clear_button_with_previous_input(self):
+        self.app.num_button_click(3) # press 3
+        self.app.edit_button_click("Clear") # press clear
+        self.assertEqual(self.app.inputs, [])
+        self.assertEqual(self.app.textbox.get("1.0", "end"), "\n")
+
+
 
 if __name__ == '__main__':
     unittest.main() 
